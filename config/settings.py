c8ffe84c458f2bad3b62bba7685f48b85a71be4c
CodeWithClinton <clintonmatics@gmail.com>
2026-04-22 13:16:20 +0100

    updated cors

--- config/settings.py
+++ config/settings.py
@@ -195,6 +195,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://app.mycode.fit"
 ]
 
 # # Allow temporary ngrok frontends without editing settings on every tunnel change.
